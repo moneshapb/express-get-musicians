3eac90386ff12b92d613ecb89b78cce6b533fcf7
moneshapb <monesha.phillipsbrown@barclays.com>
2023-02-20 16:52:16 +0000

    finish

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
-
+app.get("/musician", async (req, res) => {
+    const musician = await Musician.findAll();
+    res.json(list);
+})
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
